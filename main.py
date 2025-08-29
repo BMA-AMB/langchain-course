60b937548808c66aaa595cbc689da32aa4e7dc7a
BMA <bru_amancio@hotmail.com>
2025-08-28 23:31:10 -0300

    react search agent bma

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
 from dotenv import load_dotenv
-from langchain_core.prompts import PromptTemplate
-from langchain_openai import ChatOpenAI
-from langchain_ollama import ChatOllama
 
 load_dotenv()
 
+from langchain import hub
+from langchain.agents import AgentExecutor
+from langchain.agents.react.agent import create_react_agent
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+
+tools = [TavilySearch()]
+llm = ChatOpenAI(model="gpt-4")
+react_prompt = hub.pull("hwchase17/react")
+agent = create_react_agent(
+    llm=llm,
+    tools=tools,
+    prompt=react_prompt
+)
+
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=False)
+chain = agent_executor
+
 def main():
-    print("Hello from langchain-course!")
-
-    information = """
-Angelina Jolie Voight (Los Angeles, 4 de junho de 1975) é uma atriz, cineasta e ativista humanitária americana. Estreou no cinema ao lado de seu pai, Jon Voight, em Lookin' to Get Out (1982); porém, a carreira dela começou a sério uma década mais tarde, quando participou do filme de baixo orçamento Cyborg 2 (1993), seguido de seu primeiro papel principal em uma grande produção em Hackers (1995). Posteriormente, foi escalada para estrelar os telefilmes biográficos George Wallace (1997), pelo qual ganhou seu primeiro Prêmio Globo de Ouro de Melhor Atriz Coadjuvante em Televisão e recebeu uma indicação ao Prêmio Emmy do Primetime para Melhor Atriz Coadjuvante em minissérie ou telefilme, e Gia (1998), vencendo novamente o Globo de Ouro, só que, desta vez, na categoria de Melhor Atriz em Minissérie ou Filme para Televisão. Em 1999, recebeu elogios por parte dos críticos especializados por sua interpretação como Lisa Rowe no filme Girl, Interrupted, pelo qual ganhou o Oscar de Melhor Atriz Coadjuvante.
-"""
-    summary_template = """
-    given the information {information} about a person I want you to create:
-    1. A short summary
-    2. Two interesting facts about them
-"""
-
-    summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+    result = chain.invoke(
+        input={
+            "input":"search for 3 job posting for an ai engineer using langchain in the bay area on linkedin and list their details",
+        }
     )
-
-    #llm = ChatOpenAI(temperature=0, model="gpt-4")
-    llm = ChatOllama(temperature=0, model="gemma3:270m") 
-    chain = summary_prompt_template | llm
-    response = chain.invoke(input={"information":information})
-    print(response.content)
-
+    print(result)
 
 if __name__ == "__main__":
     main()
